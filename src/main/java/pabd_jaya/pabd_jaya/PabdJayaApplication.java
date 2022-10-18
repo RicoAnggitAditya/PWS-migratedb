123c68889b2c143b29f11f76b3ca23c83b3703ce
RicoAnggitAditya <ricoanggit01@gmail.com>
2022-10-18 18:06:56 +0700

    connect database

---
+++ src/main/java/pabd_jaya/pabd_jaya/PabdJayaApplication.java
@@ -0,0 +1,13 @@
+package pabd_jaya.pabd_jaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PabdJayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PabdJayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pabd_jaya/pabd_jaya/PabdJayaApplicationTests.java
@@ -0,0 +1,13 @@
+package pabd_jaya.pabd_jaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabdJayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
